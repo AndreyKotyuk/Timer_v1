e8bd2e17cdac6aeb682b2ca69bac910b7c3b8bf7
AndreyKotyuk <andreikotuk1@gmail.com>
2015-12-10 19:14:03 +0200

    Update Form1.Designer.cs

--- WayToTheFuture-TimerFuture/Form1.Designer.cs
+++ WayToTheFuture-TimerFuture/Form1.Designer.cs
@@ -440,7 +440,6 @@
             // 
             // timer1
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Timer
             // 
